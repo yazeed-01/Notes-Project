96b588005999a4cf17a09912b27cca79f4dfd561
yazeed-01 <yazeedam2004@gmail.com>
2024-02-12 10:55:15 -0800

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const allButtons = document.querySelectorAll('.searchBtn');
+    const searchBar = document.querySelector('.searchBar');
+    const searchInput = document.getElementById('searchInput');
+    const searchClose = document.getElementById('searchClose');
+  
+    for (var i = 0; i < allButtons.length; i++) {
+      allButtons[i].addEventListener('click', function() {
+        searchBar.style.visibility = 'visible';
+        searchBar.classList.add('open');
+        this.setAttribute('aria-expanded', 'true');
+        searchInput.focus();
+      });
+    }
+  
+    searchClose.addEventListener('click', function() {
+      searchBar.style.visibility = 'hidden';
+      searchBar.classList.remove('open');
+      this.setAttribute('aria-expanded', 'false');
+    });
+  
+  
+  });
+  document.getElementById('new-visit').addEventListener('click', function() {
+    history.replaceState(null, null, location.href);
+    window.onpopstate = function () {
+      history.go(1);
+    };
+  });
---
+++ server/routes/main.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/POST');
+
+/**
+ * GET /
+ * HOME
+*/
+router.get('', async (req, res) => {
+  try {
+    const locals = {
+      title: "Notes",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render('index', { 
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * GET /
+ * Post :id
+*/
+router.get('/post/:id', async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: data.title,
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    }
+
+    res.render('post', { 
+      locals,
+      data,
+      currentRoute: `/post/${slug}`
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * POST /
+ * Post - searchTerm
+*/
+router.post('/search', async (req, res) => {
+  try {
+    const locals = {
+      title: "Seach",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+        { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+      ]
+    });
+
+    res.render("search", {
+      data,
+      locals,
+      currentRoute: '/'
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+/**
+ * GET /
+ * About
+*/
+router.get('/about', (req, res) => {
+  res.render('about', {
+    currentRoute: '/about'
+  });
+});
+router.get('/done', (req, res) => {
+  res.render('registerdone', {
+    currentRoute: '/registerdone'
+  });
+});
+
+
+
+
+module.exports = router;
